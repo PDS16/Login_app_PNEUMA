b0a22d16b5110acaf16ef41214fa9f6d0914f897
PDS16 <200050137@iitb.ac.in>
2022-07-01 15:24:27 +0530

    test3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const {pool} = require("./dbConfig.js")
 const PORT = process.env.PORT || 4000;
-const session = require("express-session");
+const session = require("cookie-session");
 const flash = require("express-flash");
 const path = require("path")
 app.set("view engine","html");
